67e57e30bfbf30264874abe361a887ee089f60d3
Venkatesh Kannan <zhac358@live.rhul.ac.uk>
2020-03-12 11:20:28 +0000

    calibration before arbitrator starts

--- src/drivearound/Driver.java
+++ src/drivearound/Driver.java
@@ -7,15 +7,19 @@ import lejos.hardware.motor.EV3LargeRegulatedMotor;
 import lejos.hardware.port.MotorPort;
 import lejos.hardware.port.SensorPort;
 import lejos.hardware.sensor.EV3UltrasonicSensor;
+import lejos.robotics.SampleProvider;
 import lejos.robotics.subsumption.Arbitrator;
 import lejos.robotics.subsumption.Behavior;
+import lejos.utility.Delay;
 
 public class Driver {
 
-	/************/
-	public static float DISTANCE_MAX = 0.085f;
-	public static float DISTANCE_MIN = 0.055f;
-	/************/
+	/****************************************/
+	public static final float DISTANCE_DIFFERENCE = 0.2f;
+	
+	public static float DISTANCE_MAX;
+	public static float DISTANCE_MIN;
+	/****************************************/
 	
 	public static void main(String[] args) {
 		LCD.drawString("Initialising...", 2, 2);
@@ -26,7 +30,26 @@ public class Driver {
 	
 		MotorContainer container = new MotorContainer(mLeft, mRight);
 		EV3UltrasonicSensor distanceSensor = new EV3UltrasonicSensor(SensorPort.S1);
-				
+		
+		LCD.clear();
+		LCD.drawString("Callibrate", 2, 2);
+		Button.ENTER.waitForPressAndRelease();
+		
+		LCD.clear();
+		LCD.drawString("Place the robot", 1, 2);
+		
+		float sample[] = new float[1];
+
+		do {
+			SampleProvider provider = distanceSensor.getDistanceMode();
+			provider.fetchSample(sample, 0);
+			
+			LCD.clear();
+			LCD.drawString("" + sample[0], 2, 2);
+			Delay.msDelay(500);
+			
+		} while (Button.ENTER.isUp());		
+		
 		LCD.clear();
 		LCD.drawString("Press Enter", 2, 2);
 		Button.ENTER.waitForPressAndRelease();
