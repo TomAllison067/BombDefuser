54208a139adc2ca2977a73ad57059a5478604227
tomlaurenceallison@gmail.com <tomlaurenceallison@gmail.com>
2020-03-27 14:40:16 +0000

    Comments, and removed unnecessary TestMusicVirtual.java

--- src/music/MusicPlayer.java
+++ src/music/MusicPlayer.java
@@ -3,14 +3,26 @@ import java.io.File;
 
 import lejos.hardware.Sound;
 
+/**
+ * Thread to play audio files given a filename.
+ * @author Tom Allison
+ *
+ */
 public class MusicPlayer extends Thread {
 	private String fileName;
 	
+	/**
+	 * Construct a new thread given a certain Music setting
+	 * @param music the Music enum given
+	 */
 	public MusicPlayer(Music music) {
 		this.fileName = music.getFileName();
 	}
 	
-	@Override
+	/**
+	 * Calls playTune(fileName) to actually play the music.
+	 * Stops the music by being interrupted by the MusicContainer class.
+	 */
 	public void run() {
 		try {
 			while(true) {
@@ -19,7 +31,12 @@ public class MusicPlayer extends Thread {
 		} catch (InterruptedException e) {
 		}
 	}
-		
+	
+	/**
+	 * Method to actually play the music.
+	 * @param fileName the filename of the audio file
+	 * @throws InterruptedException when interrupted - allowing us to stop the music
+	 */
 	public void playTune(String fileName) throws InterruptedException {
 		int time = Sound.playSample(new File(fileName));
 		Thread.sleep(time);
--- src/music/TestMusicVirtual.java
+++
@@ -1,8 +0,0 @@
-package music;
-public class TestMusicVirtual {
-	public static void main(String[] args) {
-		MusicContainer music = new MusicContainer();
-		music.setMusic(Music.THEME_MUSIC);
-		music.playMusic(10000);
-	}
-}
