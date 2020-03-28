1c5255ce83054902130df291d0a084826fb83c89
tomlaurenceallison@gmail.com <tomlaurenceallison@gmail.com>
2020-03-28 12:28:08 +0000

    Comments

--- src/music/MusicPlayer.java
+++ src/music/MusicPlayer.java
@@ -5,7 +5,10 @@ import lejos.hardware.Sound;
 import lejos.utility.Delay;
 
 /**
- * Thread to play audio files given a filename.
+ * MusicPlayer can start a thread that plays audio files, or nothing. It can also play the exit program tones with playExitSound().
+ * To use it, call putMusicOn(Music music). If no music thread is running then a thread will start and music will be played, 
+ * otherwise if the argument is different to what is currently playing then the music will change.
+ * To silence it, call putMusicOn(null).
  * @author Tom Allison
  *
  */
@@ -19,8 +22,8 @@ public class MusicPlayer implements Runnable {
 	final static int NOTE_LENGTH = 250;
 	
 	/**
-	 * Calls playTune(fileName) to actually play the music.
-	 * Stops the music by being interrupted by the MusicContainer class.
+	 * Calls playTune(fileName).
+	 * If musicToPlay is null, simply sleeps the thread for 1/10th second.
 	 */
 	public void run() {
 		try {
@@ -33,11 +36,12 @@ public class MusicPlayer implements Runnable {
 				
 			}
 		} catch (InterruptedException e) {
+			// Do nothing - simply keep looping
 		}
 	}
 	
 	/**
-	 * Method to actually play the music.
+	 * Called by run() to actually play the audio file.
 	 * @param fileName the filename of the audio file
 	 * @throws InterruptedException when interrupted - allowing us to stop the music
 	 */
@@ -46,7 +50,11 @@ public class MusicPlayer implements Runnable {
 		Thread.sleep(time);
 	}
 	
-	
+	/**
+	 * Starts a thread that plays the music given as an argument (or nothing if the argument is null).
+	 * If a thread is already running and this method is called with a different argument, the thread is interrupted to switch to the new music.
+	 * @param music the music to play
+	 */
 	public static void putMusicOn(Music music) {
 		if (myThread == null) {
 			musicToPlay = music;
@@ -60,6 +68,9 @@ public class MusicPlayer implements Runnable {
 		}
 	}
 	
+	/**
+	 * Simply plays some tones. Called by behaviors which deal with exiting the program (BatteryBehavior, DefusalComplete, EscapeButtonBehavior, Retreat)
+	 */
 	public static void playExitSound() {
 		Sound.playTone(F5, NOTE_LENGTH);
 		Delay.msDelay(NOTE_LENGTH);
