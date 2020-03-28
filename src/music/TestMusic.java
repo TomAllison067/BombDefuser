363af43b0266db4f57db1b82a2d48a07b7ac65bf
tomlaurenceallison@gmail.com <tomlaurenceallison@gmail.com>
2020-03-28 11:57:00 +0000

    Removed TestMusic.java

--- src/music/TestMusic.java
+++
@@ -1,9 +0,0 @@
-package music;
-
-public class TestMusic {
-	public static void main(String[] args) {
-		MusicContainer music = new MusicContainer();
-		music.setMusic(Music.THEME_MUSIC);
-		music.playMusic(10000);
-	}
-}
